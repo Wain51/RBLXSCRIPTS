05f8055d8a0393f76c2008f4663ae73eac98f7c1
Wain51 <137408498+Wain51@users.noreply.github.com>
2023-10-20 21:31:35 +0300

    Create Simple_TweenTeleport.lua

---
+++ Simple_TweenTeleport.lua
@@ -0,0 +1,5 @@
+local CFrameEnd = CFrame.new() -- Place your coords in here
+local Time = 5 -- Time in seconds
+local tween =  game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time), {CFrame = CFrameEnd})
+tween:Play()
+tween.Completed:Wait()
